6a71f49cc089d0f8be0ed529ba9c2cded8593d6a
Eric Schles <ericschles@gmail.com>
2021-10-09 11:56:48 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# https://medium.com/swlh/setup-and-run-web-app-on-terraform-using-docker-c8e045da8860
+
+resource "docker_container" "nginx" {
+  image = "hello_world:develop"
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
